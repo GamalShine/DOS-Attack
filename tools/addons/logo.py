8ca79527b4671fc68694c9678ab4e4e3092c189c
GamalShine <119243187+GamalShine@users.noreply.github.com>
2022-11-29 02:13:20 +0800

    Update logo.py

--- tools/addons/logo.py
+++ tools/addons/logo.py
@@ -9,18 +9,7 @@ def show_logo() -> None:
 
     Returns:
         None
-    """
-    
-hijau="\033[1;92m "
-putih="\033[1;97m"
-abu="\033[1;90m"
-kuning="\033[1;93m"
-ungu="\033[1;95m"
-merah="\033[1;91m"
-biru="\033[1;96m"
-
-    {putih}\tGAMAL IN HERE BABYY :D\t
-    
+    """  
     logo = """
   ▒█████   ██▒   █▓▓█████  ██▀███   ██▓     ▒█████   ▄▄▄      ▓█████▄
   ▒██▒  ██▒▓██░   █▒▓█   ▀ ▓██ ▒ ██▒▓██▒    ▒██▒  ██▒▒████▄    ▒██▀ ██▌
@@ -33,10 +22,6 @@ biru="\033[1;96m"
       ░ ░        ░     ░  ░   ░         ░  ░    ░ ░        ░  ░   ░    
                 ░                                               ░     
   """
-{putih}[{biru}•{putih}] {biru}Author {putih}   : Gamal Musthofa
-{putih}[{biru}•{putih}] {abu}GitHub {putih}   : https://github.com/GamalShine
-{putih}[{biru}•{putih}] {merah}You{putih}Tube {putih}  : Saranin dong
-{putih}[{biru}•{putih}] {ungu}Instagram {putih}: @gamalmust
 
     CRED2 = "\33[91m"
     print(CRED2 + logo + CRED2)
